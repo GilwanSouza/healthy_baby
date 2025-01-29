aa5a18346f32f07c99032dc94514cc40ca25be21
Gilwan Souza <111159533+GilwanSouza@users.noreply.github.com>
2025-01-29 14:38:34 -0300

    0.2.2

---
+++ healthybaby/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'telefone', 'password1', 'password2']
--- healthybaby/views.py
+++ healthybaby/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import CustomUserForm
+from django.contrib.auth import login
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Loga o usuário automaticamente
+            return redirect('home')  # Redireciona para a página inicial
+    else:
+        form = CustomUserForm()
+
+    return render(request, 'healthybaby/templates/usuarios/register.html', {'form': form})
