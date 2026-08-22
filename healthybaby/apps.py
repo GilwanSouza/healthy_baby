9143552ab63f45fea4bf6d650a4fe14b2bd8927c
GilwanSouza <gilwansanttos@gmail.com>
2026-08-22 16:29:08 -0300

    add user

--- healthybaby/apps.py
+++ healthybaby/apps.py
@@ -1,5 +1,20 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+def create_default_user(sender, **kwargs):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    
+    USERNAME = 'admin'
+    EMAIL = 'admin@admin.com'
+    PASSWORD = 'senha_padrao_123'
+
+    if not User.objects.filter(username=USERNAME).exists():
+        User.objects.create_superuser(username=USERNAME, email=EMAIL, password=PASSWORD)
 
 class HealthybabyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'healthybaby'
+
+    def ready(self):
+        post_migrate.connect(create_default_user, sender=self)
